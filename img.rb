17eeed92e70241b7245978c070045d148923f3e4
anton lomov <anton.lomov@elcmarketing.com>
2015-09-30 11:26:20 +0200

    create repo

---
+++ img.rb
@@ -0,0 +1,26 @@
+require 'csv'
+require "net/http"
+require "uri"
+
+start_time = Time.now
+def check(url)
+  uri = URI(url)
+  res = Net::HTTP.get_response(uri)
+  res.code
+end
+
+result = []
+array_of_data = CSV.read('sourcefile1.csv', headers:false, col_sep:';')
+
+
+ array_of_data.each do |url|
+  if check(url[0]) == '404'
+    result << [url[1], url[2]]
+  end
+end
+
+puts "Pocet chyb obrazkov: #{result.length}"
+result.each {|item| puts "#{item[1]} - #{item[0]}"}
+end_time = Time.now
+puts "Time: #{end_time - start_time}"
+
