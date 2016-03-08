90103ffa8df7aebb23e34812b987fba64e8a6ac4
anton lomov <anton.lomov@elcmarketing.com>
2016-03-08 11:14:52 +0100

    refactor csv

--- bin/img_csv.rb
+++ bin/img_csv.rb
@@ -13,17 +13,6 @@ result = 0
 pictures = 0
 array_of_data = CSV.read('sourcefiles/sourcefile.csv', headers:false, col_sep:';')
 
-# File.open('missing_images_new.txt','w') { |f|
-#   array_of_data.each do |url|
-#     if check(url[0]) == '404'
-#       f.puts "#{url[2]}, #{url[1]}"
-#       result += 1
-#     end
-#     pictures += 1
-#   end
-# }
-
-# def export_csv(array_of_data)
   CSV.open('missing_images.csv', 'w', { :encoding => 'utf-8', :col_sep => ';' }) do |csv_object|
     array_of_data.each do |url|
       if check(url[0]) == '404'
@@ -34,10 +23,7 @@ array_of_data = CSV.read('sourcefiles/sourcefile.csv', headers:false, col_sep:';
     end
   end
   puts "CSV finished"
-  # puts ""
-# end
 
-# export_csv(array_of_data)
 puts "Celk pocet obrazkov: #{pictures}"
 puts "Pocet chyb obrazkov: #{result}"
 end_time = Time.now
